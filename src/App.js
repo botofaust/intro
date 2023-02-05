a63b97d04420e367d6e754be6d8edebbb77f4e94
botofaust <faust_max@mail.ru>
2023-02-05 17:41:08 +0500

    0.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BrowserPosition from './BrowserPosition';
+
+const TextInput = React.forwardRef(({ name, onChange }, ref) => (
+  <input type="text" ref={ref} name={name} onChange={onChange}/>
+))
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.positionRef = React.createRef();
+    this.refGetPosition = React.createRef();
+  }
+
+  state = {
+    textInput: "",
+  }
+
+  requestGismeteoPosition = () => {
+    const geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + this.positionRef.current.value + '&limit=5&appid=4f145577e286f86edf377185d8af97c2';
+    axios.get(geocodingAPI)
+        .then(res => {
+          console.log(res);
+          let result = '';
+          for (let r of res.data) {
+            console.log(r);
+            result += r.country + ' ' + r.state + '\n';
+          };
+          this.setState({ textInput: result });
+          console.log(result);
+        })
+        .catch(err => {
+          // this.setState({ textInput: err.message, });
+        })
+  }
+  
+  getBrowserPosition = () => {
+    this.refGetPosition.current.getPosition();
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <TextInput name="test" ref={this.positionRef} onChange={this.onInputChange}/>
+        <button onClick={this.getBrowserPosition}>click</button>
+        <BrowserPosition ref={this.refGetPosition}/>
+        <div>{this.state.textInput}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
